6fc955c44e2e61a6fa506ab724f4ff7ecd59427f
macklellan <mcclellanr15@students.ecu.edu>
2019-01-27 17:43:09 -0500

    Basic placeholder website html for autodeployment testing...

---
+++ upload.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Ditto</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+
+	<body>
+		<?php
+		echo "<h1>Upload Succesful</h2>";
+		?>
+	</body>
+
+
+	</body>
+</html>
