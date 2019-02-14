f3ca3aed31900db4cde48de5b83c7ddf0a297793
macklellan <mcclellanr15@students.ecu.edu>
2019-02-13 20:22:34 -0500

    Trying form from https://www.w3schools.com/php/php_file_upload.asp

--- upload.php
+++ upload.php
@@ -1,32 +1,94 @@
+<html>
+<head>
+    <title>Ditto</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        function display_filenames() {
+            let x = document.getElementById("uploadinput").files;
+            let y = "";
+            for (let z = 0; z < x.length; z++){
+                y += "<div class=" + "\"alert alert-info\" role=\"alert\">" + x[z].name + " selected." + "</div>";
+            }
+            document.getElementById('filenames-output').innerHTML = y;
+        }
+        function display_success() {
+            let b = "";
+            b += "<div class=" + "\"alert alert-success\" role=\"alert\"><strong>Success!</strong>" + " File has been uploaded." + "</div>";
+            document.getElementById('filenames-output').innerHTML = b;
+        }
+        function display_error() {
+            let c = "";
+            c += "<div class=" + "\"alert alert-danger\" role=\"alert\"><strong>Failure!</strong>" + " File has NOT been uploaded." + "</div>";
+            document.getElementById('filenames-output').innerHTML = c;
+        }
+    </script>
+</head>
+
+<body>
+<h1>  Ditto Drive  </h1>
+
+<br>
+
+<div class="row">
+    <div class="form-group col-md-6" id="filenames-output" title="Files">
+<!--        <div class="alert alert-danger" role="alert">-->
+<!--            No files selected.-->
+<!--        </div>-->
+    </div>
+    <div class="form-group col-md-6">
+        <ul>
+            <li>Sent file: <?php echo $_FILES['fileToUpload']['name'];  ?>
+            <li>File size: <?php echo $_FILES['fileToUpload']['size']/1000;  ?> KB
+            <li>File type: <?php echo $_FILES['fileToUpload']['type']; ?>
+        </ul>
+
+    </div>
+</div>
+
+<br><br><br><br><br>
+
+<div class="row">
+    <div class="form-group col-md-12">
+        <form action="" method="POST" enctype = "multipart/form-data">
+            <input id="uploadinput" name="fileToUpload" oninput="display_filenames()" type="file" multiple/>
+            <p>Drag your files here or click in this area.</p>
+            <button type="submit">Upload</button>
+        </form>
+    </div>
+</div>
+
 <?php
-$target_dir = ".\\Uploads\\Testing";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+if(isset($_FILES['fileToUpload'])){
+    $errors= array();
+    $file_name = $_FILES['fileToUpload']['name'];
+    $file_size = $_FILES['fileToUpload']['size'];
+    $file_tmp = $_FILES['fileToUpload']['tmp_name'];
+    $file_type= $_FILES['fileToUpload']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+    if($file_size > 2097152){
+        $errors[]="File size must be < 2 MB";
+    }
+
+    // Check if file already exists
+    if (file_exists("uploads/$file_name")) {
+        $errors[]="Sorry, file already exists.";
+    }
+
+    if(empty($errors)==true){
+        move_uploaded_file($file_tmp,"uploads/$file_name");
+        echo '<script>display_success();</script>';
+    }else{
+        echo '<script>display_error();</script>';
+        echo 'fail';
     }
 }
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+?>
+
+
+</body>
+</html>
