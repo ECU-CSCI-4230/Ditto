5904833bef55edc50924d4c838b099e73ee9a708
macklellan <mcclellanr15@students.ecu.edu>
2019-02-15 19:15:43 -0500

    Added multiple file upload support Fixed alerts for multiple file uploads

--- js/dittoj.js
+++ js/dittoj.js
@@ -1,18 +1,31 @@
+
+
+
+// ALERTS USER OF ALL SELECTED FILES TO BE UPLOADED
 function display_filenames() {
     let x = document.getElementById("uploadinput").files;
-    let y = "";
+    let text = "";
     for (let z = 0; z < x.length; z++){
-        y += "<div class=" + "\"alert alert-info\" role=\"alert\">" + x[z].name + " selected." + "</div>";
+        text += "<div class=" + "\"alert alert-info\" role=\"alert\">" + x[z].name + " selected." + "</div>";
     }
-    document.getElementById('filealert').innerHTML = y;
+    document.getElementById('filealert').innerHTML = text;
 }
-function display_success() {
-    let b = "";
-    b += "<div class=" + "\"alert alert-success\" role=\"alert\"><strong>Success!</strong>" + " File has been uploaded." + "</div>";
-    document.getElementById('filealert').innerHTML = b;
+
+// ALERTS USER OF A SUCCESSFUL UPLOAD. APPENDS STRING PARAMETER TO SENT ALERT.
+function display_success(successtext) {
+    let text = "";
+    text += "<div class=" + "\"alert alert-success\" role=\"alert\"><strong>Success!</strong>" + " File has been uploaded. " + successtext + "</div>";
+    document.getElementById('filealert').innerHTML += text;
 }
+
+// ALERTS USER OF AN UNSUCCESSFUL UPLOAD. APPENDS STRING PARAMETER TO SENT ALERT.
 function display_error(errortext) {
-    let c = "";
-    c += "<div class=" + "\"alert alert-danger\" role=\"alert\"><strong>Failure!</strong>" + " File has NOT been uploaded. " + errortext + "</div>";
-    document.getElementById('filealert').innerHTML = c;
+    let text = "";
+    text += "<div class=" + "\"alert alert-danger\" role=\"alert\"><strong>Failure!</strong>" + " File has NOT been uploaded. " + errortext + "</div>";
+    document.getElementById('filealert').innerHTML += text;
+}
+
+
+function display_upload_stats() {
+
 }
--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@
 <div class="row">
     <div class="form-group col-md-12">
         <form action="" method="POST" enctype = "multipart/form-data">
-            <input id="uploadinput" name="fileToUpload" oninput="display_filenames()" type="file" multiple/>
+            <input id="uploadinput" name="fileToUpload[]" oninput="display_filenames()" type="file" multiple/>
             <p>Drag your files here or click in this area.</p>
             <button type="submit">Upload</button>
         </form>
@@ -39,22 +39,30 @@
 
 if(isset($_FILES['fileToUpload'])){
     $err = 0;
-    $file_name = $_FILES['fileToUpload']['name'];
-    $file_size = $_FILES['fileToUpload']['size'];
-    $file_tmp = $_FILES['fileToUpload']['tmp_name'];
-//    $file_type= $_FILES['fileToUpload']['type'];
-//    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+    $total = count($_FILES['fileToUpload']['name']);
 
-    if (file_exists("uploads/$file_name")) {
-        echo '<script>display_error("' . $file_name . ' A file with this name exists already.");</script>';
-        $err = 1;
-    }else if($file_size > 8388608){
-        echo '<script>display_error("' . $file_name . ' is ' . $file_size/1048576 . ' Mb... Max size is 8 Mb");</script>';
-        $err = 1;
-    }else if ($err == 0) {
-        move_uploaded_file($file_tmp,"uploads/$file_name");
-        echo '<script>display_success();</script>';
+    $msg = "<script>";
+    for( $i=0 ; $i < $total ; $i++ ) {
+
+        $file_name = $_FILES['fileToUpload']['name'][$i];
+        $file_size = $_FILES['fileToUpload']['size'][$i];
+        $file_tmp = $_FILES['fileToUpload']['tmp_name'][$i];
+//      $file_type= $_FILES['fileToUpload']['type'];
+//      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+        if (file_exists("uploads/$file_name")) {
+            $msg .= "display_error(\"" . $file_name . " A file with this name exists already.\");";
+            $err = 1;
+        } else if ($file_size > 8388608) {
+            $msg .= 'display_error("' + $file_name . ' is ' . $file_size / 1048576 . ' Mb... Max size is 8 Mb");';
+            $err = 1;
+        } else if ($err == 0) {
+            move_uploaded_file($file_tmp, "uploads/$file_name");
+            $msg .= 'display_success("' . $file_name . ' ' . $file_size / 1048576 . ' Mb "); display_upload_stats();';
+        }
     }
+    $msg .= '</script>';
+
+    echo $msg;
 }
 ?>
 
