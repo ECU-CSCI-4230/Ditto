3f825704d358adddf11a1bc9607248c90b3d6f9b
Olivia Gomez <gomezo16@stdents.ecu.edu>
2019-02-20 16:14:53 -0500

    Uploade the front-end files

---
+++ Front-End/script.js
@@ -0,0 +1,15 @@
+// Show the Sign In Page
+function showSignIn() {
+  var x = document.getElementById("SignOrReg");
+  x.style.display = "none";
+  var y = document.getElementById("signIn");
+  y.style.display = "block";
+}
+
+// Show the Reg Page
+function showReg() {
+  var x = document.getElementById("SignOrReg");
+  x.style.display = "none";
+  var y = document.getElementById("reg");
+  y.style.display = "block";
+}
