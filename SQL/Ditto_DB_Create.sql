adc6a6fbbe0dbbdb1de2a5e57d0462513129b805
dpalmieri1 <palmierid16@students.ecu.edu>
2019-02-26 14:41:09 -0500

    Fixing the database creation script
    Linux appaently cares much more about capitilazation than windows does.

--- SQL/Ditto_DB_Create.sql
+++ SQL/Ditto_DB_Create.sql
@@ -3,14 +3,14 @@ drop table User;
 drop table File;
 
 CREATE TABLE User (
-    User_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    User_ID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Username VARCHAR(18) NOT NULL,
     Password VARCHAR(32) NOT NULL,
     Email VARCHAR(50)
 );
 
 CREATE TABLE File (
-    File_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    File_ID INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     File_Path VARCHAR(100) NOT NULL,
     File_Type VARCHAR(6) NOT NULL,
     Last_Modified DATE NOT NULL,
@@ -18,17 +18,15 @@ CREATE TABLE File (
 );
 
 CREATE TABLE FileShare (
-    User_ID INT(8) UNSIGNED NOT NULL,
-    File_ID INT(8) UNSIGNED NOT NULL,
+    User_ID INT(10) UNSIGNED NOT NULL,
+    File_ID INT(10) UNSIGNED NOT NULL,
     Permission TINYINT NOT NULL,
-    
     PRIMARY KEY (User_ID , File_ID),
-    
     FOREIGN KEY (User_ID)
         REFERENCES User (User_ID)
         ON DELETE CASCADE,
     FOREIGN KEY (File_ID)
-        REFERENCES FIle (File_ID)
+        REFERENCES File (File_ID)
         ON DELETE CASCADE
 );
 
@@ -37,3 +35,4 @@ describe File;
 describe FileShare;
 
 show tables;
+SHOW CREATE TABLE FileShare;
