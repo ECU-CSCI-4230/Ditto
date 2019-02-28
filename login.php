1759ecc195b10692727459454373e35d471edcc7
macklellan <mcclellanr15@students.ecu.edu>
2019-02-28 16:26:38 -0500

    fixed error

--- login.php
+++ login.php
@@ -1,24 +1,28 @@
 <?php
 session_start(); // Starting Session
-$error=''; // Variable To Store Error Message
-if (isset($_POST['submit'])) {
-    if (empty($_POST['username']) || empty($_POST['password'])) {
-        $error = "Username or Password is invalid";
+$error = "Begin Error Message: "; // Variable To Store Error Message
+if (isset($_POST['login'])) {
+    if (empty($_POST['uname']) || empty($_POST['psw'])) {
+        echo "Username or Password is empty";
     }
     else
     {
+        echo "Doing Stuff";
 // Define $username and $password
-        $username=$_POST['username'];
-        $password=$_POST['password'];
+        $username=$_POST['uname'];
+        $password=$_POST['psw'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
         $conn = mysqli_connect("localhost", "root", "admin", "Ditto_Drive");
+
 // To protect MySQL injection for Security purpose
         $username = stripslashes($username);
         $password = stripslashes($password);
         $username = mysqli_real_escape_string($username);
         $password = mysqli_real_escape_string($password);
+
 // SQL query to fetch information of registerd users and finds user match.
         $query = $conn->query("select User_ID from User where Password='$password' AND Username='$username'");
+
         $rows = mysqli_num_rows($query);
         if ($rows == 1) {
             $res = $query->fetch_assoc();
@@ -27,7 +31,9 @@ if (isset($_POST['submit'])) {
         } else {
             $error = "Username or Password is invalid";
         }
-        mysqli_close($connection); // Closing Connection
+        mysqli_close($conn); // Closing Connection
     }
 }
+
+echo $error;
 ?>
