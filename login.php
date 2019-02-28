9f8eff2ec7b18d9970fa0672814dd1f4939bd73a
macklellan <mcclellanr15@students.ecu.edu>
2019-02-28 16:48:00 -0500

    fixed error again #2

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<html>
+<head>
+    <title>Ditto Drive</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link href='.\style.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+<div class="container" id="signIn">
+    <form action="login.php" method="post">
+        <div class="row">
+            <center><input id="info" type="text" placeholder="Enter Username" name="uname" required></center>
+            <div class="row">
+                <center><input id="info" type="password" placeholder="Enter Password" name="psw" required></center>
+            </div>
+            <div class="row">
+                <center><input type="submit" class="btn btn-primary" value="Submit" name="login"></center>
+            </div>
+        </div>
+    </form>
+</div>
+
 <?php
 session_start(); // Starting Session
 $error = "Begin Error Message: "; // Variable To Store Error Message
@@ -39,3 +66,6 @@ if (isset($_POST['login'])) {
 
 echo $error;
 ?>
+
+</body>
+</html>
