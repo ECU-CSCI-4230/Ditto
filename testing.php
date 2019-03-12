0bac9ccf3551a7e2b0fa722953ff06fbd5488cd2
dpalmieri1 <palmierid16@students.ecu.edu>
2019-03-12 10:22:36 -0400

    DB Bugging

--- testing.php
+++ testing.php
@@ -56,5 +56,17 @@ if ($conn->multi_query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+    }
+} else {
+    echo "0 results<br>";
+}
+
 $conn->close();
 ?>
