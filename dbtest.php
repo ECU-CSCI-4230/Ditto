f1439534ee3edb046a58951a2ceedb70324a4a6e
Josh Carpenter <carpenterjo14@students.ecu.edu>
2019-03-12 15:38:13 -0400

    Working on Database password problem.

--- dbtest.php
+++ dbtest.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', 'password');
 define('DB_NAME', 'Ditto_Drive');
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
