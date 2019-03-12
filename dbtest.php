da88b3adc7c629076272d68a1dd9a630e2d27f52
Josh Carpenter <carpenterjo14@students.ecu.edu>
2019-03-12 15:44:35 -0400

    Working on Database password problem.

--- dbtest.php
+++ dbtest.php
@@ -8,6 +8,5 @@ $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 // Check connection
 if($conn === false){
-    echo"Password used $DB_PASSWORD";
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
