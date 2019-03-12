e297e59c36f804da2d8d310f6043fdeb5191791f
dpalmieri1 <palmierid16@students.ecu.edu>
2019-03-12 10:08:13 -0400

    DB Bugging

--- login.php
+++ login.php
@@ -76,7 +76,7 @@ if (isset($_POST['login'])) {
                 $error = "Username or Password is invalid ";
             }*/
 
-            if (mysqli_num_rows($result) > 0) {
+            if (mysqli_num_rows($query) > 0) {
                 // output data of each row
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "id: " . $row["User_ID"] . " - User: " . $row["Username"] . "<br>";
