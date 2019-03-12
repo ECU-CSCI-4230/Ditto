7edfa44676719e7cad3c864d841ff95dfb24dae9
Josh Carpenter <carpenterjo14@students.ecu.edu>
2019-03-12 15:51:28 -0400

    Working on Database password problem.

--- dbtest.php
+++ dbtest.php
@@ -1,11 +1,12 @@
+//this file exists just to test database connection with mysqli
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
+define('DB_USERNAME', 'josh');
+define('DB_PASSWORD', 'jcc15241711');
 define('DB_NAME', 'Ditto_Drive');
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
+
 // Check connection
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
