49af162aa4cdd54e2d4e0bffe21c7649800e5ccd
macklellan <mcclellanr15@students.ecu.edu>
2019-03-13 18:26:53 -0400

    Fixing exclusion until authentication.

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ include('session.php');
 session_start();
 if(!isset($_SESSION['login_user'])){
     header("Location:login.php");
+} else {
+    header("Location:mainDrive.html");
 }
 ?>
 <!DOCTYPE html>
