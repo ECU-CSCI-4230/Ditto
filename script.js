b6afbaa45985d1db3ca2765a07b2d1a6ed183b07
macklellan <mcclellanr15@students.ecu.edu>
2019-03-13 22:10:01 -0400

    REGISTRATION PAGE Login recognition added!

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function showReg() {
   var y = document.getElementById("reg");
   y.style.display = "block";
 }
+
+function loginSuccess() {
+    let text = "";
+    text += "<div class=" + "\"alert alert-success\" role=\"alert\"><strong>Success!</strong>" + " LOGIN SUCCESSFUL "  + "</div>";
+    document.getElementById('reg').innerHTML = text;
+}
